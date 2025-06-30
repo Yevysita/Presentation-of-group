33a940928dec872f400e98fc77dddb61f72f598b
Yevysita <valyavskaeva@gmail.com>
2025-06-30 16:45:32 +0300

    comment

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+let seconds = 0
+let minutes = 0
+let timer_field = document.querySelector(".timer")
+
+setInterval(function(){
+    seconds += 1
+
+    timer_field.innerHTML = "Time: " + seconds +"sec"
+
+    if(seconds % 60 == 0){
+        seconds = 0
+        minutes += 1   
+            
+    }
+    if (minutes > 0){
+        timer_field.innerHTML = "Time: " + minutes + " min " + seconds + " sec " 
+    }
+    
+
+}, 1000)
 
